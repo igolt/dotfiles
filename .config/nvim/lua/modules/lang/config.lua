0d60d7732fb5bba62748b17d36970842a07ece15
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2021-06-19 15:37:14 -0300

    nvim-treesitter plugin.

---
+++ .config/nvim/lua/modules/lang/config.lua
@@ -0,0 +1,26 @@
+local config = {}
+
+config.nvim_treesitter = function()
+  require'nvim-treesitter.configs'.setup {
+    highlight = {
+      enable = true,
+      disable = {'lua'},
+    },
+    indent = {
+      enable = true,
+      disable = {'lua', 'c', 'cpp'}
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        }
+      }
+    }
+  }
+end
+return config
