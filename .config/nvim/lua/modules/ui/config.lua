17bdede2c680c4eb0bef4dadcfc3fea80aa64a45
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2021-10-03 21:07:09 -0300

    neovim: Update nvim-tree config.

--- .config/nvim/lua/modules/ui/config.lua
+++ .config/nvim/lua/modules/ui/config.lua
@@ -1,19 +1,8 @@
 local config = {}
 
 function config.nvim_tree()
-  -- On Ready Event for Lazy Loading work
-  require("nvim-tree.events").on_nvim_tree_ready(function()
-    vim.cmd("NvimTreeRefresh")
-  end
-  )
-  vim.g.nvim_tree_follow = 1
   vim.g.nvim_tree_hide_dotfiles = 1
   vim.g.nvim_tree_indent_markers = 1
-  vim.g.nvim_tree_bindings = {
-    {key = {'l'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('edit')<CR>"};
-    {key = {'s'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('vsplit')<CR>"};
-    {key = {'i'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('split')<CR>"};
-  }
   vim.g.nvim_tree_icons = {
     default =  '',
     symlink =  '',
@@ -25,6 +14,32 @@ function config.nvim_tree()
       untracked = "★",
     },
   }
+
+  require('nvim-tree').setup {
+    disable_netrw = true,
+    open_on_tab = true,
+    hijack_cursor = true,
+    update_cwd = true,
+    lsp_diagnostics = true,
+    update_focused_file = {
+      enable = true,
+    },
+    view = {
+      mappings = {
+        costom_only = false,
+        list = {
+          {key = {'l'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('edit')<CR>"};
+          {key = {'s'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('vsplit')<CR>"};
+          {key = {'i'}, cb = "<cmd>lua require'nvim-tree'.on_keypress('split')<CR>"};
+        }
+      }
+    }
+  }
+
+  -- On Ready Event for Lazy Loading work
+  require("nvim-tree.events").on_nvim_tree_ready(function()
+    vim.cmd("NvimTreeRefresh")
+  end)
 end
 
 function config.nvim_bufferline()
